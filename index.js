b4181971fcb5e6ffda6c8664a0cc2e59e2bfbc93
Your Name <you@example.com>
2018-02-04 14:56:44 -0500

    express setup

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.send('resume stuff');
+});
+
+
+app.listen('5000', () => {
+    console.log('The server is running on localhost:5000');
+});
