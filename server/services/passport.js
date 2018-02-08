ef6e74cb34ce1f61994db95ea6c687deadbeac71
aadevelop <abusahamed@gmail.com>
2018-02-08 18:02:14 -0500

    syntax update to read users gmail messages

--- server/services/passport.js
+++ server/services/passport.js
@@ -2,12 +2,11 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
+const Google = require('googleapis');
+const  Gmail = Google.gmail('v1');
+const OAuth2 = Google.auth.OAuth2;
+
 const User = mongoose.model('users');
-
-const google = require('googleapis');
-let  gmail = google.gmail('v1');
-let OAuth2 = google.auth.OAuth2;
-
 
 
 passport.serializeUser((user, done) => {
@@ -28,36 +27,33 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
+
       const alreadyUser = await User.findOne({googleId: profile.id});
-	// get email test
-	
+      
+      // get email test
 
-console.log(accessToken + "   TOKEN   " + refreshToken);	
+      console.log(accessToken + "   TOKEN   " + refreshToken);	
 
-	let authclient = new OAuth2(keys.googleClientID,keys.googleClientSecret,"/");
-	authclient.setCredentials({access_token:accessToken,refresh_token:refreshToken});
-	gmail.users.messages.list({userId:'me',auth:authclient},function err(err,response){
- 	console.log("ERRROR " + "   " + err);	
-	console.log("RESPONSE" + "   "  + response);
-	
- 	});
+      const authClient = new OAuth2(keys.googleClientID, keys.googleClientSecret, "/");
+      authClient.setCredentials({ access_token: accessToken, refresh_token: refreshToken });
+      Gmail.users.messages.list({ userId: 'me', auth:authclient }, function err(err, response){
+      console.log("ERRROR " + "   " + err);
+      console.log("RESPONSE" + "   "  + response);
+      });
 
 
       console.log(profile);
       if(alreadyUser) {
-	let authclient = new OAuth2(keys.googleClientID,keys.googleClientSecret,"/");
-console.log(accessToken + "   TOKEN   " + refreshToken);	
+      let authclient = new OAuth2(keys.googleClientID,keys.googleClientSecret, "/");
+      console.log(accessToken + "   TOKEN   " + refreshToken);	
 
-	authclient.setCredentials({access_token:accessToken,refresh_token:refreshToken});
-	gmail.users.messages.list({userId:'me',auth:authclient,maxResults:10},function err(err,response){
- 	console.log("ERRROR " + "   " + err);	
-	console.log(response);
+      authclient.setCredentials({ access_token: accessToken, refresh_token: refreshToken });
+      gmail.users.messages.list({ userId:'me', auth: authclient, maxResults:10 }, function err(err, response){
+      console.log("ERRROR " + "   " + err);	
+      console.log(response);
+ 	    });
 
-	
- 	});
-
-
-        return done(null, alreadyUser);
+      return done(null, alreadyUser);
       }
 
       const user = await new User({ 
