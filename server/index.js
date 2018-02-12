3ae6ec065163eea121ce8431b52b7f5b11cf04ac
aadevelop <abusahamed@gmail.com>
2018-02-11 19:30:00 -0500

    cookie-session

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-const app = require('express')();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const keys = require('./config/keys');
+const app =           require('express')();
+const mongoose =      require('mongoose');
+const bodyParser =    require('body-parser');
+const cookieSession = require('cookie-session');
+const passport =      require('passport');
+const keys =          require('./config/keys');
 
 require('./models/User');
- require('./services/passport');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+    })
+); 
 
 app.use(passport.initialize());
 app.use(passport.session());
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,18 +2,28 @@ const passport = require('passport');
 
 module.exports = app => {
     app.get('/', (req, res) => {
-        res.send('resume stuff');
+        res.send('its me');
     });
-	
 
+    app.get('/api/user', (req, res) => {
+        res.send(req.user);
+    });
 
 // add permissions for email reading and writing
-app.get('/auth/google',passport.authenticate('google', { scope: [ 'https://mail.google.com/' ,'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/gmail.modify'],accessType: 'offline', approvalPrompt: 'force' }));
+    app.get('/auth/google', 
+    passport.authenticate('google', { 
+        scope: [
+        'https://mail.google.com/' ,
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/gmail.modify']
+        ,accessType: 'offline', approvalPrompt: 'force' 
+    }));
       
   app.get('/auth/google/callback', 
     passport.authenticate('google'),
     function(req, res) {
-      //If the oauth goes through Successful redirect root.
+     //If the oauth goes through Successful redirect root.
       res.redirect('/');
     }); 
 
